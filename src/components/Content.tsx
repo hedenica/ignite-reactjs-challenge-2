b4edcb46892e1dcb940dde4d95dcf0eb0659d339
hedenicapedrosa <hedenica@hotmail.com>
2021-03-17 00:15:45 -0300

    ✨  first component divison

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface ContentProps {
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+  selectedGenre: string;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span>{selectedGenre}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
